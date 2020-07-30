663a84753dc0dfb21c51b29648d3ce075df89eef
Arman <arman.zrb@gmail.com>
2020-07-30 17:40:35 +1000

    Check clamby is working before scanning the file

--- app/jobs/document_service/document_scan_job.rb
+++ app/jobs/document_service/document_scan_job.rb
@@ -1,7 +1,18 @@
 class DocumentService::DocumentScanJob < SharedModules::ApplicationJob
   class ScanFailure < StandardError; end
 
+  def clamby_is_ready?
+    # This keeps running clamby safe till it returns true once
+    @ready ||= Clamby.safe?(Rails.root.join('Gemfile').to_s)
+  end
+
   def perform(document)
+    10.times do
+      next if clamby_is_ready?
+      sleep 5
+    end
+    raise "Clamby not ready!" unless clamby_is_ready?
+
     file = download_file(document)
     status = case Clamby.safe?(file)
              when true then document.mark_as_clean!
