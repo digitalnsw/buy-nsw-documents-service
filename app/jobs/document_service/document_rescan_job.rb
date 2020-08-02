282b27fa038ba23826a7148d300a368aa884d0bd
Arman <arman.zrb@gmail.com>
2020-08-02 15:18:25 +1000

    Removing the unwanted arg from perfom

--- app/jobs/document_service/document_rescan_job.rb
+++ app/jobs/document_service/document_rescan_job.rb
@@ -1,7 +1,7 @@
 class DocumentService::DocumentRescanJob < SharedModules::ApplicationJob
   class ScanFailure < StandardError; end
 
-  def perform(document)
+  def perform
     DocumentService::Document.unscanned.each(&:scan_file) if doc.updated_at < 15.minute.ago
   end
 end
