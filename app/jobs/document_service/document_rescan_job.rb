8d3fbc17c2ffc4c931f01a9d766402d31d736095
Arman <arman.zrb@gmail.com>
2020-08-02 16:17:14 +1000

    Fix for rescan job

--- app/jobs/document_service/document_rescan_job.rb
+++ app/jobs/document_service/document_rescan_job.rb
@@ -2,6 +2,8 @@ class DocumentService::DocumentRescanJob < SharedModules::ApplicationJob
   class ScanFailure < StandardError; end
 
   def perform
-    DocumentService::Document.unscanned.each(&:scan_file) if doc.updated_at < 15.minute.ago
+    DocumentService::Document.unscanned.each do |doc|
+      doc.scan_file if doc.updated_at < 15.minute.ago
+    end
   end
 end
